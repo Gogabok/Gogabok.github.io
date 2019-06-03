7db9eaee3ab8e4cbb147a5ef945e1054b669603b
Gogabok <gogabok123@gmail.com>
2019-06-03 12:51:27 +0300

    Right-track-1

---
+++ right-track-1/modal-3/1-4/index.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Right-Track</title>
+	<link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i|Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&amp;subset=cyrillic" rel="stylesheet">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/slick-theme.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/slick.css">
+	<link rel="stylesheet" href="css/simple.css">
+	<link rel="stylesheet" href="css/jquery.arcticmodal-0.3.css">
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<section class="first" id="first">
+		<div class="left-circle"></div>
+		<header class="header" id="header">
+			<div class="two-black"></div>
+			<div class="two-cubs">
+				<img src="img/two-cubs.png" alt="two-cubs">
+			</div>
+			<div class="container">
+				<div class="first-contant">
+					<div class="logo">
+						<a href="index.html"><img src="img/logo.png" alt="logotype Right-Track"></a>
+					</div>
+					<div class="title">
+						<p class="subTitle">Новая программа от университета Right Track</p>
+						<h1 class="mainTitle"><span>Развитие </span>предпринимателей</h1>
+						<p class="textTitle">Пробиваем стеклянный потолок. Кратный рост прибыли через стратегию на 144 года и прокачку 10-ти предпринимательских навыков.</p>
+					</div>
+					<div class="icons d-flex">
+						<div class="icon">
+							<img src="img/icon1.png" alt="date">
+							<p class="icon-text">
+							25 - 28 апреля 2019 г.</p>
+						</div>
+						<div class="icon">
+							<img src="img/icon2.png" alt="clock">
+							<p class="icon-text">
+							11:00-19:00</p>
+						</div>
+						<div class="icon">
+							<img src="img/icon3.png" alt="adress">
+							<p class="icon-text">
+							г.Москва</p>
+						</div>
+					</div>
+					<div class="button">
+						<a href="#" class="btn">Оставить заявку</a>
+					</div>
+					<div class="right-circle"></div>
+					<div class="black-circle"></div>
+					<div class="dotted"></div>
+					<div class="lines"></div>
+				</div>
+			</div>
+		</header>
+	</section>
+	<section class="second" id="second">
+		<div class="container">
+			<div class="ball">
+				<img src="img/02ball.png" alt="ball">
+			</div>
+			<div class="small-dotted">
+				<img src="img/small-dotted.png" alt="small-dotted">
+			</div>
+			<div class="two-rounds">
+				<img src="img/two-rounds.png" alt="two-rounds">
+			</div>
+			<div class="second-contant">
+				<h2 class="second-title">Дисклеймер</h2>
+				<p class="second-text">Мы знаем, как оформляются продающие лендинги. Мы верим, что вы тоже это знаете. А ещё мы верим, что вам не <br> часто предлагают изучить законы кратного роста и построить стратегию для вашей династии на 144 года, <br> воплощение которой будет основано на развитии предпринимательских навыков. </p>
+
+				<p class="second-text">Предложение хорошее, материал проверенный, мы надеемся на вашу сознательность. <br>
+				Наличие собеседования перед курсом, отсекает тех, кому интенсив не поможет или не нужен. </p>
+			</div>
+			<div class="video">
+				<img src="img/play-oval.png" alt="play" class="play-oval">
+				<img src="img/video.jpg" alt="video">
+				<p class="video-text">Модуль 3 - развитие предпринимательских навыков и стратегия на 144 года</p>
+			</div>
+		</div>
+	</section>
+	<section class="third" id="third">
+		<div class="third-copy">
+			<img src="img/third-copy.png" alt="third-copy">
+		</div>
+		<div class="container">
+			<div class="third-contant">
+				<h2 class="third-title"><span>О нас</span> пишут СМИ</h2>
+				<div class="ourpartners d-flex">
+					<img src="img/RusBase.png" alt="RusBase" class="ourpartner-first">
+					<img src="img/news.png" alt="news">
+				</div>
+				<div class="banner-slider-thumbs">
+						<a href="#" class="banner-slider-thumb-item">
+								<div class="img-wrap">
+									<img src="img/play2.png" alt="play" class="play-overlay">
+								</div>
+								<p class="text">Интервью с Александром Бакеевым, <br> на радио Mediametrics. </p>
+							</a>
+						<a href="#" class="banner-slider-thumb-item">
+								<span class="img-wrap">
+									<img src="img/play2.png" alt="play" class="play-overlay">
+								</span>
+								<span class="text">Интервью с Александром Бакеевым, <br> на радио Mediametrics. </span>
+							</a>
+						<a href="#" class="banner-slider-thumb-item">
+								<span class="img-wrap">
+									<img src="img/play2.png" alt="play" class="play-overlay">
+								</span>
+								<span class="text">Интервью с Александром Бакеевым, <br>на радио Mediametrics. </span>
+							</a>
+						<a href="#" class="banner-slider-thumb-item">
+								<span class="img-wrap">
+									<img src="img/play2.png" alt="play" class="play-overlay">
+								</span>
+								<span class="text">Интервью с Александром Бакеевым, <br>на радио Mediametrics. </span>
+							</a>
+						<a href="#" class="banner-slider-thumb-item">
+								<span class="img-wrap">
+									<img src="img/play2.png" alt="play" class="play-overlay">
+								</span>
+								<span class="text">Интервью с Александром Бакеевым,<br> на радио Mediametrics. </span>
+							</a>
+						<a href="#" class="banner-slider-thumb-item">
+								<span class="img-wrap">
+									<img src="img/play2.png" alt="play" class="play-overlay">
+								</span>
+								<span class="text">Интервью с Александром Бакеевым,<br> на радио Mediametrics. </span>
+							</a>
+						<a href="#" class="banner-slider-thumb-item">
+								<span class="img-wrap">
+									<img src="img/play2.png" alt="play" class="play-overlay">
+								</span>
+								<span class="text">Интервью с Александром Бакеевым,<br> на радио Mediametrics. </span>
+							</a>
+						<a href="#" class="banner-slider-thumb-item">
+								<span class="img-wrap">
+									<img src="img/play2.png" alt="play" class="play-overlay">
+								</span>
+								<span class="text">Интервью с Александром Бакеевым, <br>на радио Mediametrics. </span>
+							</a>
+					</div>
+					<p class="third-subtitle">Серия интервью с основателем RIGHT TRACK - <br> Александром Бакеевым, на радио MEDIAMETRICS.</p>
+				<div class="third-ball">
+					<img src="img/third-ball.png" alt="third-ball">
+				</div>
+			</div>
+		</div>
+		<div class="third-dotted">
+			<img src="img/small-dotted.png" alt="small-dotted">
+		</div>
+	</section>
+	<div class="yellow-circle">
+		<img src="img/yellow-crcle.png" alt="circle">
+	</div>
+	<section class="fourth" id="fourth">
+		<div class="container">
+			<div class="fourth-rounds">
+				<img src="img/two-rounds.png" alt="rwo-rounds">
+			</div>
+			<div class="fourth-dotted">
+				<img src="img/small-dotted.png" alt="small-dotted">
+			</div>
+			<div class="fourth-ball">
+				<img src="img/third-ball.png" alt="third-ball">
+			</div>
+			<div class="right-oval">
+				<img src="img/right-circle.png" alt="right-circle">
+			</div>
+			<div class="fourth-balls">
+				<img src="img/third-ball.png" alt="third-ball">
+			</div>
+			<div class="fourth-content">
+				<h2 class="fourth-title"><span>Для кого</span> эта лекция</h2>
+				<div class="persons">
+					<div class="person-item d-flex">
+						<div class="person-photo">
+							<img src="img/photo1.png" alt="photo1" class="persons-for">
+						</div>
+						<div class="personal-wrap">
+							<h3 class="person-title">Инвесторам</h3>
+							<p class="personal-text">Вы узнаете, как передать свой предпринимательский опыт <br> детям и замотивировать их наращивать семейный капитал</p>
+						</div>
+					</div>
+					<div class="person-item d-flex">
+						<div class="personal-wrap personal-wrap2">
+							<h3 class="person-title">Собственников бизнеса</h3>
+							<p class="personal-text">Вы узнаете, кому передать компанию в управление, <br> если текущие наследники не подходят на эту роль. </p>
+						</div>
+						<div class="person-photo person-photo2">
+							<img src="img/photo2.png" alt="photo2" class="photo2 persons-for">
+						</div>
+					</div>
+					<div class="person-item d-flex">
+						<div class="person-photo">
+							<img src="img/photo3.png" alt="photo3" class="persons-for">
+						</div>
+						<div class="personal-wrap">
+							<h3 class="person-title person-title-third">Для проектных <br> менеджеров/руководителей,<br> в т.ч. корпоративных акселераторов</h3>
+							<p class="personal-text">Вы узнаете, как повысить свой социальный статус и развить <br> навыки, которые помогут вам зарабатывать больше.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<div style="display: none;">
+    <div class="box-modal" id="modalWindow">
+        <div class="box-modal_close arcticmodal-close">Х</div>
+        <iframe class="video-frame" src="https://www.youtube.com/embed/D3qrt0EgVd8?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+</div>
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.arcticmodal-0.3.min.js"></script>
+	<script src="js/slick.min.js"></script>
+	<script src="js/script.js"></script>
+</body>
+</html>
---
+++ right-track-1/modal-3/index.php
@@ -0,0 +1,4 @@
+<?php
+	include "1-4/index.php";
+	include "5-8/index.php";
+?>
