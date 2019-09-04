26783cbafd5832100a9ec41cefe19eef5e85b4a8
Gogabok <gogabok123@gmail.com>
2019-09-04 10:11:29 +0300

    cngs

---
+++ projects/constructor/schemeGeneration.js
@@ -0,0 +1,541 @@
+function layoutScheme(payload) {
+    let layout = []
+    let count = payload * 10
+    if(payload === 1) {
+        for(let i = 0; i < count; i++) {
+            layout.push(
+            [
+                {
+                    title: Math.random(),
+                    src: "",
+                    rotation: 0
+                },
+                {
+                    title: Math.random(),
+                    src: "",
+                    rotation: 0
+                },
+                {
+                    title: Math.random(),
+                    src: "",
+                    rotation: 0
+                },
+                {
+                    title: Math.random(),
+                    src: "",
+                    rotation: 0
+                },
+                {
+                    title: Math.random(),
+                    src: "",
+                    rotation: 0
+                },
+                {
+                    title: Math.random(),
+                    src: "",
+                    rotation: 0
+                },
+                {
+                    title: Math.random(),
+                    src: "",
+                    rotation: 0
+                },
+                {
+                    title: Math.random(),
+                    src: "",
+                    rotation: 0
+                },
+                {
+                    title: Math.random(),
+                    src: "",
+                    rotation: 0
+                },
+                {
+                    title: Math.random(),
+                    src: "",
+                    rotation: 0
+                },
+                
+            ]
+            )
+        }
+    }
+    if(payload === 2) {
+        for (let i = 0; i < count; i++) {
+            layout.push(
+                [
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                ]
+            )
+            
+        }
+    }
+    if (payload === 3) {
+        for (let i = 0; i < count; i++) {
+            layout.push(
+                [
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                ]
+            )
+
+        }
+    }
+    if (payload === 4) {
+        for (let i = 0; i < count; i++) {
+            layout.push(
+                [
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                    {
+                        title: Math.random(),
+                        src: "",
+                        rotation: 0
+                    },
+                ]
+            )
+
+        }
+    }
+    return layout 
+}
