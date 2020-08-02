15fb33ffbd345de3ad9471523a54d3e41c35264a
Gogabok <gogabok123@gmail.com>
2020-08-02 19:55:59 +0300

    .

---
+++ other/beautyBooking/assets/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $('.services-carousel').owlCarousel({
+    loop: false,
+    margin: 16,
+    nav: false,
+    autoWidth: true,
+  });
+
+  $('.sertificates-carousel').owlCarousel({
+    loop: true,
+    margin: 16,
+    nav: false,
+    autoWidth: true,
+  });
+})
